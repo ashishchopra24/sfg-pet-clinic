7e3be936417d21496b6b171116b3763b544535bd
achopra <achopra@IN1-ACHOPRA.corp.e2open.com>
2019-06-23 00:27:43 +0530

    data model

--- Master-Project/src/main/java/com/start/project/model/Pet.java
+++ Master-Project/src/main/java/com/start/project/model/Pet.java
@@ -7,6 +7,24 @@ public class Pet {
 	PetType petType;
 	Owner owner;
 	LocalDate birthDate;
+	public PetType getPetType() {
+		return petType;
+	}
+	public void setPetType(PetType petType) {
+		this.petType = petType;
+	}
+	public Owner getOwner() {
+		return owner;
+	}
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
 	
 	
 	
